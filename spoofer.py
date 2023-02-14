46bc0448801da7f3191f224a503f0473d49a58ed
Jadengadea <jadensmail@proton.me>
2023-02-14 21:28:08 +0100

    Algorithmic Mac-address spoofer

--- spoofer.py
+++ spoofer.py
@@ -1,11 +1,15 @@
+# Path: spoofer.py
 import subprocess
 import optparse
 import re
 
+# where is there error in the code?
+
+
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC adress")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac Address)
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac Address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface, user --helpfor more info")
@@ -13,11 +17,11 @@ def get_arguments():
         parser.error("[-] Please specify a new mac, user --help for more info")
     return options
 
-    
+
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC address for " + interface + " to " + + new_ mac)
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
-    subprocess.callable(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 def get_current_mac(interface):
@@ -28,13 +32,19 @@ def get_current_mac(interface):
         return (mac_adress_search_result.group(0))
     else:
         print("[-] Could not read MAC adress.")
-
-
+        
 options = get_arguments()
 current_mac = get_current_mac(options.interface)
+change_mac(options.interface, options.new_mac)
 print("Current MAC = " + str(current_mac))
 
-# change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC adress was successfully changed to " + current_mac)
+else:
+    print("[-] MAC adress did not get changed.")
+    
+
 
 
 
